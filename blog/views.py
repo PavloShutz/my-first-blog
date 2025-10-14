d136c7690c92351cc964f4f15dceb07114eb1850
Pavlo Shutz <pavlo050706@gmail.com>
2025-10-14 09:44:09 +0300

    Post drafts and the ability to publish them.

--- blog/views.py
+++ blog/views.py
@@ -20,12 +20,10 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
@@ -36,9 +34,20 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull = True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.publish()
+    return redirect('post_detail', pk=pk)
